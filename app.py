403e97ff3ec70d6ed4df96f66a6102f49ccde041
render8755 <renderstart21@gmail.com>
2024-08-04 16:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 import requests
 from time import sleep
@@ -251,5 +252,6 @@ body{
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
