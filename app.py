ad2449c381a25f74166ae9cc010c129d09792db5
render8755 <renderstart21@gmail.com>
2024-08-10 00:14:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -266,6 +266,6 @@ def main():
     send_messages_from_file()   
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
